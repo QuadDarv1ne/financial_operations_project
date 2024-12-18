8c99c7eda92a2f909ab4de1faa6963edd0803ef7
Max Dupley <maksimqwe42@mail.ru>
2024-12-18 21:28:37 +0300

    modified:   app/main.py 	modified:   app/templates/index.html

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-# *******************************************************
-# app/main.py                                           #
-# *******************************************************
-from . import app
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-# Основной файл приложения (точка входа)
+# Создаем экземпляр Jinja2Templates, указывая директорию шаблонов
+templates = Jinja2Templates(directory="app/templates")
+
+router = APIRouter()
+
+@router.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
